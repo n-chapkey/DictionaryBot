7b112599d91de60948fe1355adfdc4e59154409f
Nicholas Chapkey <kolkachapper@gmail.com>
2020-09-08 21:28:09 -0400

    Project directory remake and added functionality to respond to ~test command
    Remade Project directory and made it respond to ~test

--- DictionaryBot/src/dictionaryPackage/Main.java
+++ DictionaryBot/src/dictionaryPackage/Main.java
@@ -1,4 +1,4 @@
-package dictionaryBotPackage;
+package dictionaryPackage;
 
 import javax.security.auth.login.LoginException;
 
@@ -10,10 +10,13 @@ import net.dv8tion.jda.api.entities.*;
 
 public class Main {
 	public static JDA jda;
+	public static String prefix = "~";
 	public static void main(String[] args) throws LoginException {
-		JDA jda = JDABuilder.createDefault("NzUzMDQ3MTY5MjYyNDIwMDE4.X1gf1A.PMlvtBzmZPXHoC5opnzxhIep-P0").build();
-		jda.getPresence().setStatus(OnlineStatus.ONLINE);
+		JDA jda = JDABuilder.createDefault("NzUzMDQ3MTY5MjYyNDIwMDE4.X1gf1A.4pxKXllODLXdw089vY_z9-TWxOA").build();
+		jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
 		jda.getPresence().setActivity(Activity.watching("Tutorials on Youtube."));
+		
+		jda.addEventListener(new Commands());
 	}
 	
 	
