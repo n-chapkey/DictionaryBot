948d510ce978cfcaa5810d57a36ad7640c965ebd
Nicholas Chapkey <kolkachapper@gmail.com>
2020-09-15 16:17:50 -0400

    Reads token from file
    Made token a seperate hidden file

--- DictionaryBot/src/dictionaryPackage/Main.java
+++ DictionaryBot/src/dictionaryPackage/Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 import javax.security.auth.login.LoginException;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
