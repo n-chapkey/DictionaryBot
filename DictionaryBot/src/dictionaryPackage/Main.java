17f2e99f5e9da9689de8a361190bd96efbdbe089
Nicholas Chapkey <kolkachapper@gmail.com>
2020-09-15 16:13:12 -0400

    Update Main.java

--- DictionaryBot/src/dictionaryPackage/Main.java
+++ DictionaryBot/src/dictionaryPackage/Main.java
@@ -1,5 +1,9 @@
 package dictionaryPackage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.AccountType;
@@ -11,8 +15,14 @@ import net.dv8tion.jda.api.entities.*;
 public class Main {
 	public static JDA jda;
 	public static String prefix = "~";
-	public static void main(String[] args) throws LoginException {
-		JDA jda = JDABuilder.createDefault("NzUzMDQ3MTY5MjYyNDIwMDE4.X1gf1A.4pxKXllODLXdw089vY_z9-TWxOA").build();
+	public static void main(String[] args) throws LoginException, FileNotFoundException {
+		
+		File tokenObj = new File("token.txt");
+		Scanner reader = new Scanner(tokenObj);
+		String token = reader.nextLine();
+		reader.close();
+		
+		JDA jda = JDABuilder.createDefault(token).build();
 		jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
 		jda.getPresence().setActivity(Activity.watching("Tutorials on Youtube."));
 		
