b5f1fb2d094d4e806ae7c230a0383d40a6a31edf
Nicholas Chapkey <kolkachapper@gmail.com>
2020-11-01 11:26:40 -0500

    Wordscraper added boilerplate
    It almost works...

--- DictionaryBot/src/dictionaryPackage/WordScraper.java
+++ DictionaryBot/src/dictionaryPackage/WordScraper.java
@@ -1,6 +1,36 @@
 package dictionaryPackage;
+import java.io.IOException;
+
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WordScraper {
+	String theWord;
+	String definition;
+	String sentence;
+	String url;
+	Document doc;
 	
+	public WordScraper(String input) {
+		theWord = input;
+		definition = "";
+		sentence = "";
+		url = "https://www.dictionary.com/browse/";
+	}
+	
+	public void scanWebsite() throws IOException {
+		doc = Jsoup.connect(url +""+ theWord).get();
+		
+		Element def = doc.select("span.messageBody").first();
+		definition = "The definition is: " + def.text();
+		String title = doc.title();
+		System.out.println(definition);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		WordScraper ws = new WordScraper("Sold");
+		ws.scanWebsite();
+	}
 }
